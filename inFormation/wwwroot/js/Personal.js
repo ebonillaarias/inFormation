04a06170f1fe89f1b9ba6d1c920469c9c03a8899
ebonillaarias <36945373+ebonillaarias@users.noreply.github.com>
2019-08-20 22:54:53 -0500

    estilos

---
+++ inFormation/wwwroot/js/Personal.js
@@ -0,0 +1,22 @@
+﻿
+
+$(document).ready(function () {
+
+    
+})
+
+
+var availableTags = [
+    "Centro costo 1",
+    "Centro costo 2",
+    "Centro costo 3",
+];
+
+
+$("#tags").autocomplete({
+    source: availableTags
+});
+
+$("#txt_start").datepicker();
+
+$("#txt_end").datepicker();
