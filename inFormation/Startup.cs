3f1c248adffae53a94170c165482d9967490186b
Jair La Serna Quiroz <jairlsq21@gmail.com>
2019-08-29 15:01:59 -0500

    Diseño Login

--- inFormation/Startup.cs
+++ inFormation/Startup.cs
@@ -58,7 +58,7 @@ namespace inFormation
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Account}/{action=Login}/{id?}");
             });
         }
     }
